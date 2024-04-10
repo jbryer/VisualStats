9703c3d76431a4948e77364c35595e6c5e426be0
Jason Bryer <jason@bryer.org>
2024-04-10 19:31:41 -0400

    Fixed UI issue for MLE.

--- R/mle_shiny.R
+++ R/mle_shiny.R
@@ -36,50 +36,47 @@ mle_shiny <- function(df, default_x, default_y, ...) {
 #' @return a Shiny UI object.
 #' @export
 mle_shiny_ui <-  function() {
-	navbarPage(
-		tabPanel(
-			'Main',
-			sidebarLayout(
-				sidebarPanel(
-					width = 3,
-					uiOutput('outcome_ui'),
-					uiOutput('predictor_ui'),
-					uiOutput('highlight_ui'),
-					hr(),
-					uiOutput('iteration_ui'),
-					checkboxInput('showOLSRegression',
-								  'Show Regression',
-								  value = TRUE)
-				),
-				mainPanel(
-					width = 9,
-					tabsetPanel(
-						tabPanel(
-							'Plots',
-							fluidRow(
-								column(6, plotOutput("scatter_plot", click = "scatter_plot_click")),
-								# column(6, plotOutput('likelihood_plot'))
-								column(6, plotOutput("parameter_plot"))
-							)
-							# plotOutput("parameter_plot")
-				 		),
-						tabPanel(
-							'Summary',
-							h4('MLE Results'),
-							verbatimTextOutput('mle_summary'),
-							h4('lm or glm Results'),
-							verbatimTextOutput('summary'),
-							h4('Point Info'),
-							verbatimTextOutput("click_info")
-						),
-						tabPanel(
-							'Likelihood Plots',
-							plotOutput('likelihood_plots', height = '600px')
-						),
-						tabPanel(
-							'Data',
-							DT::dataTableOutput('datatable')
+	fluidPage(
+		sidebarLayout(
+			sidebarPanel(
+				width = 3,
+				uiOutput('outcome_ui'),
+				uiOutput('predictor_ui'),
+				uiOutput('highlight_ui'),
+				hr(),
+				uiOutput('iteration_ui'),
+				checkboxInput('showOLSRegression',
+							  'Show Regression',
+							  value = TRUE)
+			),
+			mainPanel(
+				width = 9,
+				tabsetPanel(
+					tabPanel(
+						'Plots',
+						fluidRow(
+							column(6, plotOutput("scatter_plot", click = "scatter_plot_click")),
+							# column(6, plotOutput('likelihood_plot'))
+							column(6, plotOutput("parameter_plot"))
 						)
+						# plotOutput("parameter_plot")
+			 		),
+					tabPanel(
+						'Summary',
+						h4('MLE Results'),
+						verbatimTextOutput('mle_summary'),
+						h4('lm or glm Results'),
+						verbatimTextOutput('summary'),
+						h4('Point Info'),
+						verbatimTextOutput("click_info")
+					),
+					tabPanel(
+						'Likelihood Plots',
+						plotOutput('likelihood_plots', height = '600px')
+					),
+					tabPanel(
+						'Data',
+						DT::dataTableOutput('datatable')
 					)
 				)
 			)
